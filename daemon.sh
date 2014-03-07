4c608e37c80fae72da8585befda86ab6a4cab06e
Protospace <info@protospace.ca>
2014-03-06 18:52:22 -0700

    Added missing files from last commit

---
+++ daemon.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+# Author: Kevin Loney
+#
+# /etc/init.d/door
+#
+### BEGIN INIT INFO
+# Provides: door
+# Required-Start: $all
+# Should-Start: 
+# Required-Stop: 
+# Should-Stop:
+# Default-Start:  2 3 4 5
+# Default-Stop:   0 1 6
+# Short-Description: Control the Protospace door
+# Description: Control the Protospace door system
+### END INIT INFO
+
+# Activate the python virtual environment
+#    . /path_to_virtualenv/activate
+
+DAEMON=/home/pi/DoorControl/app.py
+
+case "$1" in
+  start)
+    echo "Starting server"
+    # Start the daemon 
+    python $DAEMON start
+    ;;
+  stop)
+    echo "Stopping server"
+    # Stop the daemon
+    python $DAEMON stop
+    ;;
+  restart)
+    echo "Restarting server"
+    python $DAEMON restart
+    ;;
+  *)
+    # Refuse to do other stuff
+    echo "Usage: /etc/init.d/door {start|stop|restart}"
+    exit 1
+    ;;
+esac
+
+exit 0
---
+++ server/__init__.py
@@ -0,0 +1,155 @@
+import os
+import sys
+import RPi.GPIO as GPIO
+import serial
+import sqlite3
+import atexit
+import time
+import logging
+
+def dict_factory(cursor, row):
+	d = {}
+	for idx, col in enumerate(cursor.description):
+		d[col[0]] = row[idx]
+	return d
+
+class DaemonApp(object):
+	def __init__(self):
+		self.stdin_path = '/dev/null'
+		self.stdout_path = '/dev/null'
+		self.stderr_path = '/dev/null'
+		self.pidfile_path =  '/var/run/door.pid'
+		self.pidfile_timeout = 5
+
+		self.running = True
+		self.log = logging.getLogger("daemon")
+
+	def __halt(self):
+		self.running = False
+
+	def run(self):
+		atexit.register(self.__halt)
+
+		self.setup()
+		while self.running:
+			try:
+				self.loop()
+			except KeyboardInterrupt:
+				break
+			except Exception as ex:
+				self.log.exception(ex)
+				break
+
+		self.stop()
+
+	def setup(self):
+		pass
+
+	def stop(self):
+		pass
+
+	def loop(self):
+		pass
+
+class App(DaemonApp):
+	def __init__(self, serial, db_file):
+		super(App, self).__init__();
+
+		self.serial = serial
+		self.db_file = db_file
+		
+		self.last_card = None
+
+	def setup(self):
+		self.log.info("starting")
+
+		GPIO.setwarnings(False)
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(17, GPIO.OUT)
+		GPIO.output(17, GPIO.LOW)
+
+	def loop(self):
+		card = self.serial.readline()
+		if not card:
+			self.last_card = None
+			return
+
+		card = card.strip()
+		if card == self.last_card or len(card) != 10:
+			return
+		self.last_card = card
+
+		self.handle_card_read(card)
+
+	def stop(self):
+		self.log.info("stopping")
+
+	def handle_card_read(self, card):
+		self.log.info("Card read: %s" % (card,))
+
+		db = sqlite3.connect(self.db_file)
+		db.row_factory = dict_factory
+
+		self.unify_serial_numbers(db, card)
+
+		# TODO flag duplicates
+		cards = self.query_cards(db, card)
+		if cards:
+			card = cards.pop(0)
+
+			# TODO merge duplicate values
+			for temp in cards:
+				pass
+
+			self.update_timestamp(db, card['serial'])
+
+			self.log.info("%s has entered the space" % (card["owner"],))
+			GPIO.output(17, GPIO.HIGH)		
+
+			time.sleep(5)
+			self.play_sound("/home/pi/soundbyte/%s" % (card['soundbyte'],))
+
+			self.log.info("Locking")
+			GPIO.output(17, GPIO.LOW)
+
+		db.close()
+
+	def unify_serial_numbers(self, db, card):
+		q = "UPDATE cards SET serial = '%s' WHERE serial = '%s'" % (card, card[::-1])
+		db.execute(q)
+
+	def update_timestamp(self, db, card):
+		q = "UPDATE cards SET first_seen = datetime('now') WHERE serial = '%s' AND first_seen IS NULL" % (card)
+		db.execute(q)
+
+		q = "UPDATE cards SET last_seen = datetime('now') WHERE serial = '%s'" % (card)
+		db.execute(q)
+
+		db.commit()
+
+	def play_sound(self, path):
+		if not (path and os.path.exists(path)):
+			return
+
+		if not pygame.mixer.music.get_busy():
+			try:
+				pygame.mixer.music.load(path)
+				pygame.mixer.music.play()
+			except:
+				self.log.warn("Failed to play sound '%s'" % (path))
+
+	def query_cards(self, db, card):
+		cur = db.cursor()
+		query = "SELECT * FROM cards WHERE serial = '%s' ORDER BY id ASC" % card
+		cur.execute(query)
+
+		cards = []
+
+		row = cur.fetchone()
+		while row:
+			cards.append(row)
+			row = cur.fetchone()
+
+		self.log.debug(cards)
+
+		return cards
